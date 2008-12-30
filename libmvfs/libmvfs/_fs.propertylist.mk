53480cb93dac4fa8f41b55873a134e63504f8e21
crosstool <crosstool@e7cd7e5e-2915-46f3-ad35-8287600d15ae>
2008-12-30 14:13:02 +0000

    git-svn-id: svn://nibiru.metux.de/public/libmvfs/trunk@1307 e7cd7e5e-2915-46f3-ad35-8287600d15ae

---
+++ libmvfs/libmvfs/_fs.propertylist.mk
@@ -0,0 +1,7 @@
+#
+# Rules for the autoconnect-fs
+#
+
+FS_SRCNAMES += propertylist_ops
+FS_LIBS     += $(HASH_LIBS)
+FS_CFLAGS   += $(HASH_CFLAGS)
---
+++ libmvfs/libmvfs/_fs.rostring.mk
@@ -0,0 +1,7 @@
+#
+# Rules for the autoconnect-fs
+#
+
+FS_SRCNAMES += rostring_ops
+FS_LIBS     += $(HASH_LIBS)
+FS_CFLAGS   += $(HASH_CFLAGS)

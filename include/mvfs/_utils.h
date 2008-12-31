98805a616c5c226c26c2eb309cce28869d5e2b03
crosstool <crosstool@e7cd7e5e-2915-46f3-ad35-8287600d15ae>
2008-12-31 02:57:11 +0000

    forked off socket-fs branch and moved away that code from trunk
    git-svn-id: svn://nibiru.metux.de/public/libmvfs/branches/socket-fs@1313 e7cd7e5e-2915-46f3-ad35-8287600d15ae

--- include/mvfs/_utils.h
+++ include/mvfs/_utils.h
@@ -46,10 +46,6 @@
 extern "C" {
 #endif
 
-/* internal utils ... DO NOT use in clients */
-int __mvfs_sock_get_line (FILE* logfile, int sock, char *buf, int buf_len, char term);
-int mvfs_decode_filetype (char t);
-
 #ifdef __cplusplus
 }
 #endif
---
+++ libmvfs/_fs.socketfs.mk
@@ -0,0 +1,7 @@
+#
+# Rules for the mixp/9P filesystem driver
+#
+
+FS_SRCNAMES += socket_fs
+FS_LIBS     +=
+FS_CFLAGS   +=

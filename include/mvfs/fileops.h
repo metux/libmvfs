c66f0aa30daf6c5a9deeab2ad0486b65cd3bc22f
Enrico Weigelt, metux IT service <weigelt@metux.de>
2009-11-05 04:21:02 +0100

    added mvfs_fs_is_regfile() inline

--- include/mvfs/fileops.h
+++ include/mvfs/fileops.h
@@ -99,6 +99,25 @@ static inline int mvfs_fs_is_dir(MVFS_FILESYSTEM* fs, const char* filename)
     }
 }
 
+// check whether given filename exists and points to an regular file
+static inline int mvfs_fs_is_regfile(MVFS_FILESYSTEM* fs, const char* filename)
+{
+    MVFS_STAT* st = mvfs_fs_statfile(fs, filename);
+    if (st == NULL)
+	return 0;
+
+    if (S_ISREG(st->mode))
+    {
+	mvfs_stat_free(st);
+	return 1;
+    }
+    else
+    {
+	mvfs_stat_free(st);
+	return 0;
+    }
+}
+
 // check whether given filename exists
 static inline int mvfs_fs_exists(MVFS_FILESYSTEM* fs, const char* filename)
 {
